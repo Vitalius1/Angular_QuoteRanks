e150e15d6c7bad94b2f8c03b764f52ec0646b5c4
VitalieBraga <vitaliebraga@gmail.com>
2017-09-18 15:03:49 -0700

    finished the assignment with the sorting done

--- src/app/quote/models/sortBy.ts
+++ src/app/quote/models/sortBy.ts
@@ -1,7 +1,11 @@
-export function sortByDesc(arr, key){
-    for(let i = 0; i < arr.length - 1; i++){
-        if(arr[i].key < arr[i+1]){
-            let temp = arr
-        }
+export function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
     }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Quote } from './models/quote';
+import { dynamicSort } from './models/sortBy';
 
 @Component({
   selector: 'app-quote',
@@ -13,20 +14,25 @@ export class QuoteComponent implements OnInit {
   onSubmit(form){
     console.log("Form Submited");
     this.quotes.push(this.quote);
+    this.quotes.sort((a, b) => b.author.localeCompare(a.author)) // sorting by a key when the value is a string.
+    this.quotes.sort(dynamicSort("-rating")); // sorting by a key when the value is a number needs an import of a function.
     this.quote = new Quote();
     form.resetForm();
   }
   upVote(idx){
     this.quotes[idx].rating += 1;
+    this.quotes.sort(dynamicSort("-rating"));
   }
   downVote(idx){
     this.quotes[idx].rating -= 1;
+    this.quotes.sort(dynamicSort("-rating"));
   }
   deleteQuote(idx){
     var temp = this.quotes[idx];
     this.quotes[idx] = this.quotes[this.quotes.length - 1];
     this.quotes[this.quotes.length - 1] = temp;
     this.quotes.pop();
+    this.quotes.sort(dynamicSort("-rating"));
   }
   ngOnInit() {
   }
